b1a7b59f84c1fc1cb3524daede98f15872204d7d
weblue3 <weblue2@gmail.com>
2021-02-26 19:17:37 +0900

    [김홍준]hello jpa 예제 학습

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,25 +4,45 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
+        //-- 엔티티 매니저 팩토리는 하나만 생성해서 애플리케이션 전체에서 공유
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
+        //-- 엔티티 매니저는 쓰레드간에 공유X(사용하고 버려야 한다.)
         EntityManager em = emf.createEntityManager();
 
+        //-- JPA의 모든 데이터 변경은 트랜잭션 안에서 실행
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
-        Member member = new Member();
-        member.setId(2L);
-        member.setName("HelloB");
+        try{
 
-        em.persist(member);
+            //-- Member findMember = em.find(Member.class, 1L);
+            //-- findMember.setName("HelloJPA");
 
-        tx.commit();
+            //-- JPQL
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(10)
+                    .getResultList();
 
-        em.close();
+            for(Member member : result){
+                System.out.println("member.name = " + member.getName());
+            }
+
+            //-- 삭제 기능
+            //-- em.remove(findMember);
+
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+
         emf.close();
 
     }
